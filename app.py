fac064b5f47011fbbc7a83d64602501ad96386af
RAMYA H P <ramyahp2004@gmail.com>
2025-11-22 10:02:10 +0530

    Change app.run to specify host and port

--- app.py
+++ app.py
@@ -513,4 +513,4 @@ def internal_error(error):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
